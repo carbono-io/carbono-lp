b956ef51b86472ae03ae76d1c2c6419726391a25
Luciana Heuko <lalalala@lalala.com>
2015-09-21 14:35:38 -0300

    [JRD - 498] estrutura da landing page - about us

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 //carregar gulp
 var gulp = require("gulp");
 var gulpLess = require("gulp-less");
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 
@@ -9,12 +10,16 @@ gulp.task("less", function () {
     
     return gulp.src('src/index.less')
         .pipe(gulpLess())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('src'));
 });
 
 
 //task para atualizar o browser 
-gulp.task("watch", function () {
+gulp.task("watch", ['less'], function () {
     
 //gulp atualiza toda vez que em qualquer diretorio, qualquer arquivo de less for alterado - no caso temos o variables e o index
     gulp.watch('src/**/*.less', ["less"]);
