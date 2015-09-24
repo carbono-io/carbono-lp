f360aa8b5804d76eca66610b493e4fd3fdcfca40
Simon <simonxd@gmail.com>
2015-09-24 17:08:48 -0300

    added translations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
     ///////////////////////
+    /////// INTERNATIONALIZATION
+    
+    var i18nextOptions = {
+        fallbackLng: 'en',
+        // Locales that are loadable
+        lngWhitelist: ['pt', 'en']
+    };
+
+    i18n.init(i18nextOptions, function(err, t) {
+      // translate everything
+      $("body").i18n();
+    });
+    
+    ///////////////////////
     ///////////////////////
     /////// FORM
 
